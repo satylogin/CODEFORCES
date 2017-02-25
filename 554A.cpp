c82aa8b61e978f99baacedd3390e4259e28f4c95
Satyarth Agrahari <satyarthagrahari@gmail.com>
2017-02-25 12:17:46 +0530

    Create 554A.cpp

---
+++ 554A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define MOD 1000000007
+#define ll long long int
+#define ld long double
+#define pb push_back
+#define mkp make_pair
+#define scan(x) scanf("%d", &x)
+
+int main()
+{
+	string s;
+	cin >> s;
+	cout << (26 * (s.size() + 1) - s.size()) << endl;
+
+	return 0;
+}
