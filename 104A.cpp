6ef62a84688f493c5a8da795b306fc7a17bf96b7
Satyarth Agrahari <satyarthagrahari@gmail.com>
2017-05-12 22:47:52 +0530

    Create 104A.cpp

---
+++ 104A.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define MOD 1000000007
+#define ll long long int
+#define ld long double
+#define pb push_back
+#define mkp make_pair
+#define pii pair<int, int> 
+#define pll pair<long long int, long long int>
+#define sci(x) scanf("%d", &x)
+#define scl(x) scanf("%lld", &x)
+#define fi first
+#define sc second
+
+int main()
+{
+	int n, x, y;
+	cin >> n;
+	if (n == 20) cout << 15 << endl;
+	else if (n <= 10 || n >= 22) cout << 0 << endl;
+	else cout << 4 << endl; 
+
+	return 0;
+}
