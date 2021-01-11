8b821a020608ea55459106234c199ef2843e7e69
Satyarth Agrahari <satyarthagrahari@gmail.com>
2021-01-11 21:46:44 +0530

    Create B - Last minute enhancements.rs

---
+++ 1466/B - Last minute enhancements.rs
@@ -0,0 +1,63 @@
+use std::collections::BTreeSet;
+
+fn main() {
+    use std::io::{self, Write};
+
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut sc = cf_scanner::Scanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let tc: usize = sc.next();
+    for _ in 0..tc {
+        let n: usize = sc.next();
+        let mut notes: Vec<i32> = (0..n).map(|_| sc.next()).collect();
+        notes.sort();
+        let mut diff_notes: BTreeSet<i32> = BTreeSet::new();
+        for x in notes {
+            if diff_notes.contains(&x) {
+                diff_notes.insert(x+1);
+            } else {
+                diff_notes.insert(x);
+            }
+        }
+        writeln!(out, "{}", diff_notes.len()).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+mod cf_scanner {
+    use std::io;
+    use std::str::{FromStr, SplitAsciiWhitespace};
+
+    pub struct Scanner<R> {
+        reader: R,
+        buf_str: Vec<u8>,
+        buf_iter: SplitAsciiWhitespace<'static>,
+    }
+
+    impl<R: io::BufRead> Scanner<R> {
+        pub fn new(reader: R) -> Scanner<R> {
+            Scanner {
+                reader,
+                buf_str: Vec::new(),
+                buf_iter: "".split_ascii_whitespace(),
+            }
+        }
+
+        pub fn next<T: FromStr>(&mut self) -> T {
+            loop {
+                if let Some(token) = self.buf_iter.next() {
+                    return token.parse().ok().expect("Failed parse");
+                }
+                self.buf_str.clear();
+                self.reader
+                    .read_until(b'\n', &mut self.buf_str)
+                    .expect("Failed read");
+                self.buf_iter = unsafe {
+                    let slice = std::str::from_utf8_unchecked(&self.buf_str);
+                    std::mem::transmute(slice.split_ascii_whitespace())
+                }
+            }
+        }
+    }
+}
