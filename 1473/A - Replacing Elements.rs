5239a1350d11f83c29be43a8ce26844ab4c45943
Satyarth Agrahari <satyarthagrahari@gmail.com>
2021-01-23 09:30:38 +0530

    Create A - Replacing Elements.rs

---
+++ 1473/A - Replacing Elements.rs
@@ -0,0 +1,54 @@
+fn main() {
+    use std::io::{self, Write};
+
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut sc = cf_scanner::Scanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let tc: usize = sc.next();
+    for _ in 0..tc {
+        let (n, d): (usize, usize) = (sc.next(), sc.next());
+        let mut array: Vec<usize> = (0..n).map(|_| sc.next()).collect();
+        array.sort();
+        let count: usize = array.iter().fold(0, |x, y| x + (*y > d) as usize);
+        writeln!(out, "{}", if count > 0 && (array[0] + array[1] > d) {"NO"} else {"YES"}).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+mod cf_scanner {
+    use std::io;
+    use std::str::{FromStr, SplitAsciiWhitespace};
+
+    pub struct Scanner<R> {
+        reader: R,
+        buf_str: Vec<u8>,
+        buf_iter: SplitAsciiWhitespace<'static>,
+    }
+
+    impl<R: io::BufRead> Scanner<R> {
+        pub fn new(reader: R) -> Scanner<R> {
+            Scanner {
+                reader,
+                buf_str: Vec::new(),
+                buf_iter: "".split_ascii_whitespace(),
+            }
+        }
+
+        pub fn next<T: FromStr>(&mut self) -> T {
+            loop {
+                if let Some(token) = self.buf_iter.next() {
+                    return token.parse().ok().expect("Failed parse");
+                }
+                self.buf_str.clear();
+                self.reader
+                    .read_until(b'\n', &mut self.buf_str)
+                    .expect("Failed read");
+                self.buf_iter = unsafe {
+                    let slice = std::str::from_utf8_unchecked(&self.buf_str);
+                    std::mem::transmute(slice.split_ascii_whitespace())
+                }
+            }
+        }
+    }
+}
