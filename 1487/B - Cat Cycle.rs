db9674aeb725211988a440d8d7ae467ed894c1c9
Satyarth Agrahari <satyarthagrahari@gmail.com>
2021-03-12 09:40:37 +0530

    Create B - Cat Cycle.rs

---
+++ 1487/B - Cat Cycle.rs
@@ -0,0 +1,59 @@
+use std::io::{self, Write};
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut sc = cf_scanner::Scanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let tc: usize = sc.next();
+    for _ in 0..tc {
+        let (n, mut k): (i64, i64) = (sc.next(), sc.next());
+        k -= 1;
+        if n & 1 == 1 {
+            let m = k / ((n - 1) / 2);
+            k = k % ((n - 1) / 2);
+            let x = (((n + 1) / 2) * m + k) % n + 1;
+            writeln!(out, "{}", x).unwrap();
+        } else {
+            writeln!(out, "{}", k % n + 1).unwrap();
+        }
+    }
+}
+
+#[allow(dead_code)]
+mod cf_scanner {
+    use std::io;
+    use std::str::{FromStr, SplitAsciiWhitespace};
+
+    pub struct Scanner<R> {
+        reader: R,
+        buf_str: Vec<u8>,
+        buf_iter: SplitAsciiWhitespace<'static>,
+    }
+
+    impl<R: io::BufRead> Scanner<R> {
+        pub fn new(reader: R) -> Scanner<R> {
+            Scanner {
+                reader,
+                buf_str: Vec::new(),
+                buf_iter: "".split_ascii_whitespace(),
+            }
+        }
+
+        pub fn next<T: FromStr>(&mut self) -> T {
+            loop {
+                if let Some(token) = self.buf_iter.next() {
+                    return token.parse().ok().expect("Failed parse");
+                }
+                self.buf_str.clear();
+                self.reader
+                    .read_until(b'\n', &mut self.buf_str)
+                    .expect("Failed read");
+                self.buf_iter = unsafe {
+                    let slice = std::str::from_utf8_unchecked(&self.buf_str);
+                    std::mem::transmute(slice.split_ascii_whitespace())
+                }
+            }
+        }
+    }
+}
