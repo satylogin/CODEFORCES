e7d8fbd2b1678c0dcaae7197b506606b5332e9bd
Satyarth Agrahari <satyarthagrahari@gmail.com>
2021-03-14 11:25:49 +0530

    Create C - Sum of Cubes.rs

---
+++ 1490/C - Sum of Cubes.rs
@@ -0,0 +1,60 @@
+use std::io::{self, Write};
+use std::collections::BTreeSet;
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut sc = cf_scanner::Scanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+
+    let cubes: BTreeSet<i64> = (1..10001 as i64).map(|i| i * i * i).collect();
+    let tc: usize = sc.next();
+    for _ in 0..tc {
+        let x: i64 = sc.next();
+        let mut contains = false;
+        for a in cubes.clone() {
+            if cubes.contains(&(x - a)) {
+                contains = true;
+                break;
+            }
+        }
+        writeln!(out, "{}", if contains {"YES"} else {"NO"}).unwrap();
+    }   
+}
+
+#[allow(dead_code)]
+mod cf_scanner {
+    use std::io;
+    use std::str::{FromStr, SplitAsciiWhitespace};
+
+    pub struct Scanner<R> {
+        reader: R,
+        buf_str: Vec<u8>,
+        buf_iter: SplitAsciiWhitespace<'static>,
+    }
+
+    impl<R: io::BufRead> Scanner<R> {
+        pub fn new(reader: R) -> Scanner<R> {
+            Scanner {
+                reader,
+                buf_str: Vec::new(),
+                buf_iter: "".split_ascii_whitespace(),
+            }
+        }
+
+        pub fn next<T: FromStr>(&mut self) -> T {
+            loop {
+                if let Some(token) = self.buf_iter.next() {
+                    return token.parse().ok().expect("Failed parse");
+                }
+                self.buf_str.clear();
+                self.reader
+                    .read_until(b'\n', &mut self.buf_str)
+                    .expect("Failed read");
+                self.buf_iter = unsafe {
+                    let slice = std::str::from_utf8_unchecked(&self.buf_str);
+                    std::mem::transmute(slice.split_ascii_whitespace())
+                }
+            }
+        }
+    }
+}
